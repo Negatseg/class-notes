94a039034b2e484d4bc52821b4f350c66f133a21
Negatseg <tseganehnegash@gmail.com>
2023-11-04 17:06:28 -0500

    Create teh javascript, express, db.json and html

---
+++ Develop/public/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// API routes
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'));
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'));
+  newNote.id = generateUniqueId(); // Implement a function to generate a unique ID
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes));
+  res.json(newNote);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// Function to generate a unique ID
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
+}
